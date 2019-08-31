74b2a33eddb17e7a909e458ba35277d48be729e6
bryanpntp <bryanpntp@users.noreply.github.com>
2019-08-30 22:28:58 -0400

    Update AWS config for API

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,4 +1,6 @@
 // Manual configuration (i.e. skipping the amplify cli assumptions) for AWS Services
+import { Auth } from 'aws-amplify'
+
 const awsmobile = {
     dev: {
         "aws_cognito_region": "us-east-2",
@@ -6,7 +8,17 @@ const awsmobile = {
         "aws_user_pools_web_client_id": "3qkoupgav6gbvs4cbebphqbdlq",
         "aws_content_delivery_bucket": "pntp-gp-ui-20190219082147-hostingbucket-dev",
         "aws_content_delivery_bucket_region": "us-east-2",
-        "aws_content_delivery_url": "http://pntp-gp-ui-20190219082147-hostingbucket-dev.s3-website.us-east-2.amazonaws.com"
+        "aws_content_delivery_url": "http://pntp-gp-ui-20190219082147-hostingbucket-dev.s3-website.us-east-2.amazonaws.com",
+
+        endpoints: [
+            {
+                name: "GlobalProbeAPI",
+                endpoint: "https://25zwa0yf5h.execute-api.us-east-2.amazonaws.com/dev/v1",
+                custom_header: async () => {
+                    return { Authorization: (await Auth.currentAuthenticatedUser()).signInUserSession.idToken.jwtToken }
+                }
+            },
+        ]
     }
 }
 
