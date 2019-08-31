d2b40cd0c1adc507bfab8f2faae02b291fc19184
bryanpntp <bryanpntp@users.noreply.github.com>
2019-08-30 22:29:44 -0400

    Add some font awesome
    Added icons preceding Add Server, Server Info, Edit, and Delete buttons
    on the dashboard.

--- src/components/ServerTable.vue
+++ src/components/ServerTable.vue
@@ -2,9 +2,11 @@
     <div>
         <b-table hover :fields="fields" :items="servers" show-empty>
             <template slot="actions" slot-scope="row">
-                <b-button :to="{ name: 'View-server', params: {serverUrl: row.item.url}}">Server Details</b-button>
-                <b-button :disabled="true">Edit</b-button>
-                <b-button :to="{ name: 'Delete-server', params: {serverUrl: row.item.url}}" variant="danger">Delete</b-button>
+                <b-button-group>
+                    <b-button :to="{ name: 'View-server', params: {serverUrl: row.item.url}}" variant="primary" ><i class="fas fa-info-circle"></i>Server Details</b-button>
+                    <b-button :disabled="true"><i class="fas fa-edit"></i>Edit</b-button>
+                    <b-button :to="{ name: 'Delete-server', params: {serverUrl: row.item.url}}" variant="danger"><i class="fas fa-trash"></i>Delete</b-button>
+                </b-button-group>
             </template>
             <template slot="empty">
                 <p class="font-italic text-info">Your are not monitoring any servers.</p>
@@ -44,6 +46,9 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
+i {
+  padding-right: 0.4em
 
+}
 </style>
